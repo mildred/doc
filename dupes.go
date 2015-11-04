ff6df1317a87904910b67f190ec068b146d92d26
Shanti Bouchez-Mongardé <shanti-pub.git@mildred.fr>
2015-11-04 15:09:49 +0100

    dupes: -d option to deduplicate identical files (make links)

--- dupes.go
+++ dupes.go
@@ -11,68 +11,75 @@ import (
 )
 
 const dupesUsage string =
-`doc dupes [DIR]
+`doc dupes [DIR...]
 
-List duplicate files under the given or the current directory. Group of
-identical files are separated by a blank line. The number of detected links is
-shown in front of each file.
+List duplicate files under the given directories (or the current directory if
+none given). Group of identical files are separated by a blank line. The number
+of detected links is shown in front of each file.
 
 Options:
 `
 
 type sameFile struct {
-  hash   []byte
-  paths  []string
-  inodes []uint64
+  hash    []byte
+  paths   []string
+  inodes  []uint64
+  devices []uint64
 }
 
 func mainDupes(args []string) {
   f := flag.NewFlagSet("dupes", flag.ExitOnError)
   opt_show_links := f.Bool("l", false, "Show group of files that share the same inode")
   opt_progress := f.Bool("p", false, "Show progress")
+  opt_dedup := f.Bool("d", false, "Deduplicate files (make links)")
   f.Usage = func(){
     fmt.Print(dupesUsage)
     f.PrintDefaults()
   }
   f.Parse(args)
-  src := f.Arg(0)
+  srcs := f.Args()
 
-  if src == "" {
-    src = "."
+  if len(srcs) == 0 {
+    srcs = append(srcs, ".")
   }
 
   dupes := map[string]sameFile{}
 
   num := 0
+  errors := 0
 
-  e := repo.Walk(src, func(path string, info os.FileInfo)error{
+  for _, src := range srcs {
+    e := repo.Walk(src, func(path string, info os.FileInfo)error{
 
-    hash, err := repo.GetHash(path, info)
-    if err != nil {
-      return err
-    }
+      hash, err := repo.GetHash(path, info)
+      if err != nil {
+        return err
+      }
 
-    sys, ok := info.Sys().(*syscall.Stat_t)
-    if ! ok {
-      sys.Ino = 0
-    }
+      sys, ok := info.Sys().(*syscall.Stat_t)
+      if ! ok {
+        sys.Ino = 0
+      }
 
-    f := dupes[string(hash)]
-    f.hash = hash
-    f.paths = append(f.paths, path)
-    f.inodes = append(f.inodes, sys.Ino)
-    dupes[string(hash)] = f
+      f := dupes[string(hash)]
+      f.hash = hash
+      f.paths = append(f.paths, path)
+      f.inodes = append(f.inodes, sys.Ino)
+      f.devices = append(f.devices, sys.Dev)
+      dupes[string(hash)] = f
 
-    num = num + 1
-    if *opt_progress {
-      fmt.Printf("\r\x1b[2K%d %s\r", num, path)
-    }
+      num = num + 1
+      if *opt_progress {
+        fmt.Printf("\r\x1b[2K%d %s\r", num, path)
+      }
 
-    return nil
-  }, func(path string, info os.FileInfo, err error)bool{
-    fmt.Fprintf(os.Stderr, "%s: %v\n", path, err.Error())
-    return true
-  })
+      return nil
+    }, func(path string, info os.FileInfo, err error)bool{
+      fmt.Fprintf(os.Stderr, "%s: %v\n", path, err.Error())
+      return true
+    })
+    errors = errors + len(e)
+  }
 
   for _, f := range dupes {
     if len(f.paths) <= 1 {
@@ -92,10 +99,44 @@ func mainDupes(args []string) {
         fmt.Printf("%s\t%d\t%s\n", hash, len(paths), path)
       }
     }
+    if len(files) > 1 && *opt_dedup {
+      err := deduplicate(f)
+      if err != nil {
+        fmt.Fprintf(os.Stderr, "%s", err.Error())
+        errors = errors + 1
+      }
+    }
   }
 
-  if e != nil {
+  if errors > 0 {
     os.Exit(1)
   }
+}
+
+func deduplicate(f sameFile) error {
+  by_dev := map[uint64][]int{}
+  for i, dev := range f.devices {
+    by_dev[dev] = append(by_dev[dev], i)
+  }
+  for _, file_list := range by_dev {
+    if len(file_list) <= 1 {
+      continue
+    }
+    first_file := file_list[0]
+    for _, cur_file := range file_list[1:] {
+      if f.inodes[first_file] == f.inodes[cur_file] {
+        continue
+      }
+      err := os.Remove(f.paths[cur_file])
+      if err != nil {
+        return err
+      }
+      err = os.Link(f.paths[first_file], f.paths[cur_file])
+      if err != nil {
+        panic(fmt.Errorf("Could not link identical file '%s' to '%s': %s", f.paths[first_file], f.paths[cur_file], err.Error()))
+      }
+    }
+  }
+  return nil
 }
 
