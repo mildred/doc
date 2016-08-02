fd4c8d0574fa17268ca276f23436f0b57563183e
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2016-08-02 17:50:01 +0200

    cleanup

--- commit/commit.go
+++ commit/commit.go
@@ -5,8 +5,6 @@ import (
 	"bytes"
 	"crypto/sha1"
 	"fmt"
-	"hash"
-	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -20,12 +18,6 @@ import (
 
 const Doccommit string = ".doccommit"
 const XattrCommit string = "user.doc.commit"
-
-type CommitFileWriter struct {
-	file   io.WriteCloser
-	path   string
-	hasher hash.Hash
-}
 
 type Entry struct {
 	Hash []byte
@@ -154,82 +146,6 @@ func readByHash(path string) (map[string][]string, error) {
 	}
 
 	return res, scanner.Err()
-}
-
-func Create(path string) (*CommitFileWriter, error) {
-	info, err := os.Lstat(path)
-	if !os.IsNotExist(err) {
-		hash, err := attrs.Get(path, XattrCommit)
-		if err != nil {
-			return nil, fmt.Errorf("%s: manually modified (%s)", path, err.Error())
-		}
-
-		if len(hash) > 0 {
-			hash2, err := repo.GetHash(path, info, false)
-			if err != nil {
-				return nil, fmt.Errorf("%s: cannot find hash (%s)", err.Error())
-			}
-
-			if !bytes.Equal(hash, hash2) {
-				return nil, fmt.Errorf("%s: file already exists and was manually modified (hash %s != %s)", path, base58.Encode(hash), base58.Encode(hash2))
-			}
-		}
-	}
-	f, err := os.Create(path)
-	if err != nil {
-		return nil, err
-	}
-
-	err = attrs.Set(path, XattrCommit, []byte{})
-	if err != nil {
-		f.Close()
-		os.Remove(path)
-		return nil, err
-	}
-
-	return &CommitFileWriter{f, path, sha1.New()}, nil
-}
-
-func (c *CommitFileWriter) Close() error {
-	err := c.file.Close()
-	if err != nil {
-		return err
-	}
-
-	info, err := os.Stat(c.path)
-	if err != nil {
-		return err
-	}
-
-	digest, err := mh.Encode(c.hasher.Sum(nil), mh.SHA1)
-	if err != nil {
-		panic(err)
-	}
-
-	err = attrs.Set(c.path, XattrCommit, digest)
-	if err != nil {
-		return err
-	}
-
-	_, err = repo.CommitFileHash(c.path, info, digest, false)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *CommitFileWriter) AddEntry(hash []byte, path string) error {
-	data := []byte(fmt.Sprintf("%s\t%s\n", base58.Encode(hash), EncodePath(path)))
-	_, err := c.hasher.Write(data)
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = c.file.Write(data)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func DecodePath(path string) string {
